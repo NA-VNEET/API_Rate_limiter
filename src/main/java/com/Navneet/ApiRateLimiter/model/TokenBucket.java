36a394ea2fcc811808fcfe7004a4e80e29ca5825
NAVNEET SINGH <nsh12727@gmail.com>
2026-03-29 11:55:27 +0530

    make the rate limiter lock free

--- src/main/java/com/Navneet/ApiRateLimiter/model/TokenBucket.java
+++ src/main/java/com/Navneet/ApiRateLimiter/model/TokenBucket.java
@@ -1,38 +1,46 @@
 package com.Navneet.ApiRateLimiter.model;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class TokenBucket {
     private final int capacity;
     private final int refillRate;
-    private AtomicInteger tokens;
-    private long lastRefillTime;
+    private AtomicLong tokens;
+    private AtomicLong lastRefillTime;
 
     public TokenBucket(int capacity, int refillRate) {
         this.capacity = capacity;
         this.refillRate = refillRate;
-        this.tokens = new AtomicInteger(capacity);
-        this.lastRefillTime = System.currentTimeMillis();
+        this.tokens = new AtomicLong(capacity);
+        this.lastRefillTime = new AtomicLong(System.currentTimeMillis());
     }
 
-    public synchronized boolean allowRequest() {
+    public boolean allowRequest() {
         refill();
 
-        if(tokens.get() > 0){
-            tokens.decrementAndGet();
-            return true;
+        while(true){
+            long currentToken = tokens.get();
+            if(currentToken < 0){
+                return false;
+            }
+            if(tokens.compareAndSet(currentToken, currentToken -1)){
+                return true;
+            }
         }
-        return false;
     }
 
     private void refill() {
         long now = System.currentTimeMillis();
-        long elapsedTime = now - lastRefillTime;
+        long lastTime = lastRefillTime.get();
+        long elapsedTime = now - lastTime;
         int tokenToAdd = (int)(elapsedTime / 1_000_000_000L * refillRate);
 
         if(tokenToAdd > 0){
-            tokens.set(Math.min(tokens.get() + tokenToAdd, capacity));
-            lastRefillTime = now;
+            long newToken = Math.min(tokens.get() + tokenToAdd, capacity);
+            if(lastRefillTime.compareAndSet(lastTime, now)){
+                tokens.set(newToken);
+            }
         }
     }
 }
